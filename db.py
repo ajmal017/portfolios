97a4a4e948070c21ebb5cf7c8f21bb533e0d5586
Ben Cutrell <bcutrell@dhcp-a-074.int.smartleaf.com>
2020-01-16 11:33:09 -0500

    updates db.py cli options

--- db.py
+++ db.py
@@ -1,5 +1,9 @@
 '''
 bcutrell13@gmail.com - January 2020
+
+Examples:
+    python3 db.py seed_file --tickers tickers.csv
+    python3 db.py seed_file --tickers IVV
 '''
 
 '''
@@ -32,10 +36,16 @@ SEED_TICKERS = ['A']
 CREATE_TABLES = 'create_tables'
 SEED_DB = 'seed_db'
 SEED_FILE = 'seed_file'
-SEED_DB_FROM_FILE = 'seed_db_from_file'
 USE_CONSOLE = 'use_console'
 
-COMMANDS = [CREATE_TABLES, SEED_DB, SEED_FILE, SEED_DB_FROM_FILE, USE_CONSOLE]
+COMMANDS = [CREATE_TABLES, SEED_DB, SEED_FILE, USE_CONSOLE]
+
+'''
+objects
+'''
+
+class DummyDB(object):
+    pass
 
 '''
 functions
@@ -60,43 +70,58 @@ def create_tables(c):
     """)
 
 
-def seed_file():
-    if os.path.exists(SEED_FILENAME):
-        df = get_batch_prices(SEED_TICKERS, merge_df=pd.read_csv(SEED_FILENAME, index_col=0))
+def seed_file(filename, tickers):
+    if os.path.exists(filename):
+        df = get_batch_prices(tickers, merge_df=pd.read_csv(filename, index_col=0))
     else:
-        df = get_batch_prices(SEED_TICKERS)
+        df = get_batch_prices(tickers)
 
-    df.to_csv(SEED_FILENAME)
+    df.to_csv(filename)
 
 
-def seed_db(c):
+def seed_db(c, tickers):
     find_security_sql = "SELECT id from securities WHERE ticker=(?)"
     insert_security_sql = "INSERT INTO securities (ticker) VALUES (?)"
 
-    insert_prices_sql = "INSERT INTO prices (date, adjusted_close, security_id) VALUES (?, ?, ?)"
+    insert_prices_sql = "INSERT OR REPLACE INTO prices (date, adjusted_close, security_id) VALUES (?, ?, ?)"
 
-    for ticker in SEED_TICKERS:
+    for ticker in tickers:
         df = get_prices(ticker)
 
         # find or create security row
-        c.execute(find_security_sql, ticker)
+        c.execute(find_security_sql, [ticker])
         security_row = c.fetchone() # returns tuple
 
         if not security_row:
-            c.execute(insert_security_sql, ticker)
+            c.execute(insert_security_sql, [ticker])
             security_id = c.lastrowid
         else:
             security_id = security_row[0]
 
         # create price rows
-        # TODO duplicate handling
         df.index = df.index.strftime("%Y-%m-%d")
         to_db = [tuple(record) + (security_id,) for record in df.to_records()]
 
         c.executemany(insert_prices_sql, to_db)
 
-def seed_db_from_file(c):
-    pass
+def get_tickers(tickers):
+    if not tickers:
+        print("Missing tickers argument")
+        exit()
+
+    try:
+        if os.path.exists(tickers):
+            with open(tickers) as f:
+                tickers = f.readlines()
+                tickers = [ticker.strip() for ticker in tickers]
+        else:
+            tickers = tickers.split(',')
+    except:
+        print("Invalid tickers {}".format(tickers))
+        exit()
+
+    return tickers
+
 
 '''
 main
@@ -105,12 +130,12 @@ main
 if __name__ == "__main__":
     my_parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
     my_parser.add_argument('command', help='One of {}'.format(', '.join(COMMANDS)))
-    my_parser.add_argument('--tickers', help='A list of tickers to get prices for. Must be comma seperated with no spaces e.g. AAPL,TSLA,JPM')
+
+    my_parser.add_argument('--tickers', help='A list of tickers to get prices for. Can be file containing a list of tickers seperated by a newline or a comma seperated list with no spaces e.g. AAPL,TSLA,JPM')
 
     args = my_parser.parse_args()
 
     command = args.command
-    tickers = args.tickers.split(',')
 
     if command not in COMMANDS:
         print("Invalid command {}".format(command))
@@ -120,8 +145,9 @@ if __name__ == "__main__":
     non-db commands
     '''
 
-    if SEED_FILE:
-        seed_file()
+    if command == SEED_FILE:
+        tickers = get_tickers(args.tickers)
+        seed_file(SEED_FILENAME, tickers)
         exit()
 
     '''
@@ -138,11 +164,9 @@ if __name__ == "__main__":
     elif command == CREATE_TABLES:
         create_tables(c)
 
-    elif SEED_DB:
-        seed_db(c)
-
-    elif SEED_DB_FROM_FILE:
-        seed_db_from_file(c)
+    elif command == SEED_DB:
+        tickers = get_tickers(args.tickers)
+        seed_db(c, tickers)
 
     # Stop DB
     conn.commit()
